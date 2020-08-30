01450f36571e141d48a13fc94960411cf0bbc488
Pendyala Harshith <harshith.pendyala777@gmail.com>
2020-08-30 19:35:45 +0530

    add

--- drowsiness_detection_Code.py
+++ drowsiness_detection_Code.py
@@ -51,7 +51,7 @@ args = vars(ap.parse_args())
 # frames the eye must be below the threshold for to set off the
 # alarm
 EYE_AR_THRESH = 0.3
-EYE_AR_CONSEC_FRAMES = 48
+EYE_AR_CONSEC_FRAMES = 45
 
 # initialize the frame counter as well as a boolean used to
 # indicate if the alarm is going off
